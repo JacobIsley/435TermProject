cdcb7ba0b157d9fd23eab605b2d559ad137da7a2
KennethHotra <97703097+KennethHotra@users.noreply.github.com>
2023-11-27 22:29:18 -0700

    Delete weird, unused import

--- AnalysisSoftware/RatingTimeAnalysis/src/main/java/cs435/RatingTimeAnalysis.java
+++ AnalysisSoftware/RatingTimeAnalysis/src/main/java/cs435/RatingTimeAnalysis.java
@@ -8,7 +8,6 @@ import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 
-import org.codehaus.janino.Java;
 import scala.*;
 
 public class RatingTimeAnalysis {
